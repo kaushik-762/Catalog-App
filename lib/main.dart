69f7cf48a5ed7138739b1ec975b927dd1d92af08
Kaushik Das <kaushik762das@gmail.com>
2022-07-26 22:42:26 +0530

    Day4partA Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'home_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,9 +10,29 @@ class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
   
   @override
+
+  //Widget-return widget(just like int and void)
+  //BuildContext-Parameter of the method "build"
+  //for method--> void bringVegetables({bool thaila=false},{int paisa=100})
+                              //{}- optional parameters 
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomePage(), //To import HomePage(Command+.)
+
+      themeMode: ThemeMode.light,
+
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        brightness: Brightness.light
+      ),
+
+      darkTheme: ThemeData(
+        brightness: Brightness.dark
+      ),
+
+
+      
+      
     );
   }
 }
